3e4981e5a31e73d0672917d5c9f9d879ba6b51a0
devhijazi <hijazi.root@gmail.com>
2021-04-09 13:47:46 -0400

    create a second component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 
 export function App () {
-  return <h1>Olá Mundo</h1>
+  return <RepositoryList/>
 }
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,26 @@
+export function RepositoryList () {
+  return (
+    <section className="repository-list">
+      <h1>Listagem de Repositórios</h1>
+    <ul>
+      <li>
+        <strong>Repo 1</strong>
+        <p>Descrição do repo 1</p>
+        <a href="#">Ver Mais</a>
+      </li>
+
+      <li>
+        <strong>Repo 1</strong>
+        <p>Descrição do repo 1</p>
+        <a href="#">Ver Mais</a>
+      </li>
+
+      <li>
+        <strong>Repo 1</strong>
+        <p>Descrição do repo 1</p>
+        <a href="#">Ver Mais</a>
+      </li>
+    </ul>
+    </section>
+  )
+}
