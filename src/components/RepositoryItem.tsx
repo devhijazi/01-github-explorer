7bd519db0edb9d777dc56bc39df44adc7d4356a8
devhijazi <hijazi.root@gmail.com>
2021-04-26 13:31:36 -0400

    transform all files jsx to tsx and add interface of types

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,9 +1,17 @@
-export function RepositoryItem(props){
-  return(
+interface RepositoryItemProps {
+  repositoryProps: {
+    name: string;
+    description: string;
+    html_url: string;
+  };
+}
+
+export function RepositoryItem(props: RepositoryItemProps) {
+  return (
     <li>
       <strong>{props.repositoryProps.name}</strong>
       <p>{props.repositoryProps.description ?? 'Sem descrição'}</p>
       <a href={props.repositoryProps.html_url}>Acessar repositório</a>
     </li>
-  )
+  );
 }
---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+import { RepositoryItem } from './RepositoryItem';
+
+import '../styles/repositories.scss';
+
+interface RepositoryState {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState<RepositoryState[]>([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/hitechline/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  }, []);
+
+  return (
+    <section className="repository-list">
+      <h1>Listagem de Repositórios</h1>
+      <ul>
+        {repositories.map(repository => {
+          return (
+            <RepositoryItem
+              key={repository.name}
+              repositoryProps={repository}
+            />
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
